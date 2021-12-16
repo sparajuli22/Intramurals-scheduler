6364004240983eaef7eeacdadec4c4c8dea2afbd
mlie23 <mlie23@my.whitworth.edu>
2021-12-16 00:48:13 -0800

    CC

--- final.py
+++ final.py
@@ -2,9 +2,11 @@ import random
 import string
 import pandas as pd
 import numpy as np
+import xlsxwriter
+
 
 # Importing data from csv file (U-REC real qualdratics input)
-data = pd.read_csv(r"C:\Users\Michael\Downloads\Team+Time+Preference_December+6,+2021_14.56 (1).csv")
+data = pd.read_csv(r"C:\Users\mlie23\Desktop\algo\Final_Project_Balsam\Team+Time+Preference_December+6,+2021_14.56 (1).csv")
 
 # Q4_1 to Q4_7 represents Monday - Sunday
 # Q7 = name of the sport 
@@ -22,6 +24,7 @@ game = [] # create list for storing specific games data
 for i in list_unique:
     game.append(data2[data2["Q7"] == i])
 
+
 for i in range(0,len(game)): # rename the days columns for the ease of iterating
     game[i] = game[i].rename(columns={
         "Q4_1":1,"Q4_2":2,"Q4_3":3,"Q4_4":4,"Q4_5":5,"Q4_6":6,"Q4_7":7}
@@ -32,22 +35,12 @@ def timeList(team, day):
     list_times = list(game[team][day])
     list_times = list(map(lambda i:[i], list_times)) # convert list to list of lists 
     dictofteams = {}
-    # print(list_teams)
-    # print(list_times)
 
     for i in range(len(list_teams)):
         dictofteams[list_teams[i]] = str(list_times[i][0]).split(",")
     return dictofteams
 
     
-# list_teams = list(game[0]['Q1'])
-# list_times = list(game[0]['Q4_4'])
-# list_times = list(map(lambda i:[i], list_times)) # convert list to list of lists 
-# check game 4 - Free Mason got fucked up
-
-# for i in range(len(list_teams)):
-
-#     print(list_teams[i],list_times[i])
 
 
 
@@ -71,16 +64,6 @@ class graph:
 def getList(dict):
     return dict.keys()
 
-print("Enter Game:")
-gameNumber = int(input())
-print("Enter Day:")
-dayNumber = int(input())
-
-newTimeList = timeList(gameNumber,dayNumber)
-#print (newTimeList)
-
-teams1 = list(getList(newTimeList))
-#print(teams1,"\n""\n")
 
 def common(list1,list2):
     for value in list1:
@@ -108,7 +91,7 @@ def check (newTimeList, teams1):
 #                        change to the number of the team they are playing against)
 #                        EX: [-1,3,-1,1] => Team no.1 will be playing with team no.3
 #            4.isPlaying: Checking if one team has played another team or not
-#                        Then it will check if a team can play another team or not
+#                        Then it will check if a team can play another team or not 
 def findPossibleMatch(graph, team, matches, isPlaying):
     for i in range(len(graph)):
         if (graph[team][i] == 1) and (isPlaying[i] == False) and (team != i):
@@ -116,51 +99,73 @@ def findPossibleMatch(graph, team, matches, isPlaying):
             if matches[i] == -1 or matches[i] == team or findPossibleMatch(graph, matches[i], matches, isPlaying):
                 matches[team] = i
                 matches[i] = team
-                return True
+                return True 
     return False
-
-
-
-def filter(graph):
-    noOfTeams = len(graph)
-    matches = [-1] * noOfTeams
-
-    for team in range(len(graph)):
-        isPlaying = [False] * noOfTeams
-        isPlaying[team] = True
-        findPossibleMatch(graph, team, matches, isPlaying)
-    # get the index of the keys in dict
-    list_index = list(newTimeList)
-    pairing = []
-    for i in matches:
-        pairing.append('No match') if i == -1 else pairing.append(list_index[i])
-    
-    return pairing
 
 def common_time (a,b):
     #This is sorted
     gametime = [c for c in a if c in b]
     return (gametime)
 
+#Writing to excel!
+count = 1
+for a in range (0,5):
+    for days in range (1,8):
+        newTimeList = timeList(a,days)
+        teams1 = list(getList(newTimeList))
+        def filter(graph):
+            noOfTeams = len(graph)
+            matches = [-1] * noOfTeams
+            for team in range(len(graph)):
+                isPlaying = [False] * noOfTeams
+                isPlaying[team] = True
+                findPossibleMatch(graph, team, matches, isPlaying)
+            # get the index of the keys in dict
+            list_index = list(newTimeList)
+            pairing = []
+            for i in matches:
+                pairing.append('No match') if i == -1 else pairing.append(list_index[i])
+            return pairing
+        final_matches = (filter(check(newTimeList, teams1)))
+        workbook = xlsxwriter.Workbook('Result'+str(count)+'.xlsx')
+        worksheet = workbook.add_worksheet()
+        count+=1
+        row = 0
+        column = 0
+        if len(final_matches)%2==1:
+            for i in range(((len(final_matches)+1)//2)):
+                if final_matches[i]!="No match":
+                    print(teams1[i], " vs ", teams1[i])
+                    worksheet.write (row,column, teams1[i]+" vs "+ final_matches[i])
+                    listoftime = common_time(newTimeList[teams1[i]], newTimeList[final_matches[i]])
+                    for time in range (len(listoftime)):
+                        worksheet.write (i,time+1, listoftime[time])
+                    column+=1
+                row+=1
+        else:
+            for i in range(((len(final_matches))//2)):
+                if final_matches[i]!="No match":
+                    print(teams1[i], " vs ", teams1[i])
+                    worksheet.write (row,column, teams1[i]+" vs "+ final_matches[i])
+                    listoftime = common_time(newTimeList[teams1[i]], newTimeList[final_matches[i]])
+                    for time in range (len(listoftime)):
+                        worksheet.write (i,time+1, listoftime[time])
+                    column+=1
+                row+=1
+        workbook.close()
+
+
+
+
+
+
+
+
+
+
     # This is shuffled
     # gametime= set(a).intersection(b)
     # return (gametime)
-
-final_matches = (filter(check(newTimeList, teams1)))
-for i in range(((len(final_matches)+1)//2)):
-    if final_matches[i]!="No match":
-        print(teams1[i], " Vs. ", final_matches[i])
-        # print(newTimeList[teams1[i]],newTimeList[final_matches[i]])
-        print (common_time(newTimeList[teams1[i]], newTimeList[final_matches[i]]))
-
-
-
-
-
-
-
-
-
 
 
 
